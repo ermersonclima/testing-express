b6920bbec2f13098886d0ac36741a8bec5eb396c
ermersonclima <el.erm.lima@gmail.com>
2024-12-29 23:26:09 -0300

    feat: create initial html file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
+import path from 'path'
 
-const app: Application = express();
-
-app.use(express.json());
-
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello, world!");
+const app = express();
+const router = express.Router();
+ 
+router.get('/',(_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
+ 
+app.use('/', router);
 
 export default app;
